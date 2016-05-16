d6667e1f421e99c0bba7aafd7e5b65eb9bc5f538
Artur <arturbumbu@gmail.com>
2016-05-16 21:35:18 +0100

    added project skeleton and all mvn dependencies

---
+++ src/main/java/com/weather/WeatherSiteApplication.java
@@ -0,0 +1,12 @@
+package com.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSiteApplication.class, args);
+    }
+}
